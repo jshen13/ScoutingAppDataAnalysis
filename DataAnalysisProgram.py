38a3efe21cf42b3bb99bc073dd59e53d982535ac
Jeffrey Shen <jeffreyshen13@gmail.com>
2019-03-20 23:22:26 -0700

    updated to 2019 deep space

--- DataAnalysisProgram.py
+++ DataAnalysisProgram.py
@@ -15,57 +15,74 @@ def sheet_setup(sheet, teamnum):
     #         AUTO
     # --------------------------------------------------
     # col 1 - match num
-    # col 2 - switch
-    # col 3 - scale
-    # col 4 - total cubes
-    # col 5 - baseline (Passed/ Not passed)
+    # col 2 - baseline
+    # col 3 - cargo 1
+    # col 4 - cargo 2
+    # col 5 - cargo 3
+    # col 6 - hatch 1
+    # col 7 - hatch 2
+    # col 8 - hatch 3
 
-    # col 7 - avg switch
-    # col 8 - avg scale
+    # col 10 - avg cargo
+    # col 11 - avg hatch
 
 
     sheet.cell(2,2).value = "AUTO"
     # AUTO
 
     sheet.cell(3, 1).value = "Match Num"
-    sheet.cell(3, 2).value = "Switch"
-    sheet.cell(3, 3).value = "Scale"
-    sheet.cell(3, 4).value = "Total"
-    sheet.cell(3, 5).value = "Baseline"
+    sheet.cell(3, 2).value = "Baseline"
+    sheet.cell(3, 3).value = "Cargo Level 1"
+    sheet.cell(3, 4).value = "Cargo Level 2/Cargo ship"
+    sheet.cell(3, 5).value = "Cargo Level 3"
+    sheet.cell(3, 6).value = "Hatch Level 1"
+    sheet.cell(3, 7).value = "Hatch Level 2"
+    sheet.cell(3, 8).value = "Hatch Level 3"
 
-    sheet.cell(3, 7).value = "AVG Switch"
-    sheet.cell(3, 8).value = "AVG Scale"
+
+
+    sheet.cell(3, 10).value = "AVG Cargo"
+    sheet.cell(3, 11).value = "AVG Hatch"
+
 
     # ----------------------------------------------------------
     #                     TELEOP
     # ----------------------------------------------------------
     # col 1 - match num
-    # col 2 - switch
-    # col 3 - vault
-    # col 4 - scale
-    # col 5 - total cubes
-    # col 6 - climb
-    # col 7 - result
-    # col 8 - notes
+    # col 2 - Balls low
+    # col 3 - Balls mid
+    # col 4 - balls high
+    # col 5 - hatch low
+    # col 6 - hatch mid
+    # col 7 - hatch high
+    # col 8 - climb
+    # col 9 - notes
+    # col 10 - total cargo
+    # col 11 - total hatch
 
-    # col 10 - avg switch
-    # col 11 - avg vault
-    # col 12 - avg scale
+    # col 13 - avg switch
+    # col 14 - avg vault
+    # col 15 - avg scale
     # input is the Excel sheet
     # TELEOP
     sheet.cell(19, 2).value = "TELEOP"
 
     sheet.cell(20,1).value = "Match Num"
-    sheet.cell(20,2).value = "Switch"
-    sheet.cell(20,3).value = "Vault"
-    sheet.cell(20,4).value = "Scale"
-    sheet.cell(20,5).value = "Total"
-    sheet.cell(20,6).value = "Climb"
-    sheet.cell(20,7).value = "Result"
-    sheet.cell(20,8).value = "Notes"
-    sheet.cell(20,10).value = "AVG Switch"
-    sheet.cell(20,11).value = "AVG Vault"
-    sheet.cell(20,12).value = "AVG Scale"
+    sheet.cell(20,2).value = "Cargo Level 1"
+    sheet.cell(20,3).value = "Cargo Level 2/Cargo ship"
+    sheet.cell(20,4).value = "Cargo Level 3"
+    sheet.cell(20,5).value = "Hatch Level 1"
+    sheet.cell(20,6).value = "Hatch Level 2"
+    sheet.cell(20,7).value = "Hatch Level 3"
+    sheet.cell(20,8).value = "Climb"
+    sheet.cell(20,9).value = "Notes"
+    sheet.cell(20,10).value = "Total Cargo"
+    sheet.cell(20,11).value = "Total Hatch"
+
+
+    sheet.cell(20,13).value = "AVG Cargo"
+    sheet.cell(20,14).value = "AVG Hatch"
+    sheet.cell(20,15).value = "AVG Climb"
 
 # --------------------------------------------------------------------------------------
 
@@ -82,36 +99,48 @@ def transfer_tele_data(data_sheet, output_sheet, d_row, o_row):
 
     # OUTPUT SHEET
     # col 1 - match num
-    # col 2 - switch
-    # col 3 - vault
-    # col 4 - scale
-    # col 5 - total cubes
-    # col 6 - climb
-    # col 7 - result
-    # col 8 - notes
+    # col 2 - Balls low
+    # col 3 - Balls mid
+    # col 4 - balls high
+    # col 5 - hatch low
+    # col 6 - hatch mid
+    # col 7 - hatch high
+    # col 8 - climb
+    # col 9 - notes
 
-    # col 10 - avg switch
-    # col 11 - avg vault
-    # col 12 - avg scale
+    # col 11 - avg switch
+    # col 12 - avg vault
+    # col 13 - avg scale
 
     #   set MATCH NUM
     output_sheet.cell(o_row, 1).value = data_sheet.cell(d_row, 3).value
-#   set SWITCH
+#   set CARGO 1
     output_sheet.cell(o_row, 2).value = data_sheet.cell(d_row, 4).value
-#   set VAULT
+#   set CARGO 2
     output_sheet.cell(o_row, 3).value = data_sheet.cell(d_row, 5).value
-#   set SCALE
+#   set CARGO 3
     output_sheet.cell(o_row, 4).value = data_sheet.cell(d_row, 6).value
-#   set TOTAL
-    output_sheet.cell(o_row, 5).value = int(data_sheet.cell(d_row, 6).value) + int(data_sheet.cell(d_row, 5).value) + int(data_sheet.cell(d_row, 4).value)
-
+#   set HATCH 1
+    output_sheet.cell(o_row, 5).value = data_sheet.cell(d_row, 7).value
+#   set HATCH 2
+    output_sheet.cell(o_row, 6).value = data_sheet.cell(d_row, 8).value
+#   set HATCH 3
+    output_sheet.cell(o_row, 7).value = data_sheet.cell(d_row, 9).value
 #   set CLIMB
-    output_sheet.cell(o_row, 6).value = data_sheet.cell(d_row, 7).value
-#   set RESULT
-    output_sheet.cell(o_row, 7).value = data_sheet.cell(d_row, 8).value
+    output_sheet.cell(o_row, 8).value = data_sheet.cell(d_row, 10).value
 
 #   set NOTES
-    output_sheet.cell(o_row, 8).value = data_sheet.cell(d_row, 9).value
+    output_sheet.cell(o_row, 9).value = data_sheet.cell(d_row, 11).value
+
+    #   set TOTAL CARGO
+    output_sheet.cell(o_row, 10).value = int(data_sheet.cell(o_row, 2).value) + int(output_sheet.cell(o_row, 3).value) \
+        + int(output_sheet.cell(o_row, 4).value)
+
+
+
+    #   set TOTAL HATCH
+    output_sheet.cell(o_row, 11).value = int(data_sheet.cell(o_row, 5).value) + int(output_sheet.cell(o_row, 6).value) \
+        + int(output_sheet.cell(o_row, 7).value)
 
 # -----------------------------------------------------------------------
 
@@ -126,30 +155,44 @@ def transfer_auto_data(data_sheet, output_sheet, d_row, o_row):
 
     # OUTPUT SHEET
     # col 1 - match num
-    # col 2 - switch
-    # col 3 - scale
-    # col 4 - total cubes
-    # col 5 - baseline (Passed/ Not passed)
+    # col 2 - baseline
+    # col 3 - cargo 1
+    # col 4 - cargo 2
+    # col 5 - cargo 3
+    # col 6 - hatch 1
+    # col 7 - hatch 2
+    # col 8 - hatch 3
 
-    # col 7 - avg switch
-    # col 8 - avg scale
+    # col 10 - avg cargo
+    # col 11 - avg hatch
 
-#   set MATCH NUM
+    #   set MATCH NUM
     output_sheet.cell(o_row, 1).value = data_sheet.cell(d_row, 3).value
-#   set SWITCH
+#   set BASELINE
     output_sheet.cell(o_row, 2).value = data_sheet.cell(d_row, 4).value
-#   set SCALE
-    output_sheet.cell(o_row, 3).value = data_sheet.cell(d_row, 6).value
+#   set CARGO 1
+    output_sheet.cell(o_row, 3).value = data_sheet.cell(d_row, 5).value
+#   set CARGO 2
+    output_sheet.cell(o_row, 4).value = data_sheet.cell(d_row, 6).value
+#   set CARGO 3
+    output_sheet.cell(o_row, 5).value = data_sheet.cell(d_row, 7).value
+#   set HATCH 1
+    output_sheet.cell(o_row, 6).value = data_sheet.cell(d_row, 8).value
+#   set HATCH 2
+    output_sheet.cell(o_row, 7).value = data_sheet.cell(d_row, 9).value
+#   set HATCH 3
+    output_sheet.cell(o_row, 8).value = data_sheet.cell(d_row, 10).value
+
 #   set TOTAL
-    output_sheet.cell(o_row, 4).value = int(output_sheet.cell(o_row, 3).value) + int(output_sheet.cell(o_row, 2).value)
+#    output_sheet.cell(o_row, 4).value = int(output_sheet.cell(o_row, 3).value) + int(output_sheet.cell(o_row, 2).value)
 
 def print_team_stat(overall_team_sheet, row, team, stat_list):
     # 1) = "Team Num"
-    # 2) = "Auto Switch AVG"
-    # 3) = "Auto Scale AVG"
-    # 4) = "Tele Switch AVG"
-    # 5) = "Tele Vault AVG"
-    # 6) = "Tele Scale AVG"
+    # 2) = "Auto cargo AVG"
+    # 3) = "Auto hatch AVG"
+    # 4) = "Tele cargo AVG"
+    # 5) = "Tele hatch AVG"
+    # 6) = "Tele climb AVG"
     # 7) = "Score"
     overall_team_sheet.cell(row, 1).value = team
     overall_team_sheet.cell(row, 2).value = stat_list[0]
@@ -158,25 +201,17 @@ def print_team_stat(overall_team_sheet, row, team, stat_list):
     overall_team_sheet.cell(row, 5).value = stat_list[3]
     overall_team_sheet.cell(row, 6).value = stat_list[4]
 
-
-
-
-
-
     # weight factors of each part that makes up the score. Can be adjusted
     # ----------------------------------------------------------------------------------------
-    a_switch_factor = 1
-    a_scale_factor = 1.5
-    t_switch_factor = 2                                 # <<<<<<   ADJUST WEIGH FACTOR
-    t_vault_factor = 1.2
-    t_scale_factor = 3.6
+    a_cargo_factor = 2
+    a_hatch_factor = 3
+    t_cargo_factor = 2                                 # <<<<<<   ADJUST WEIGH FACTOR
+    t_hatch_factor = 3
+    t_climb_factor = 1
     # ----------------------------------------------------------------------------------------
 
-
-
-
-    score = a_switch_factor * stat_list[0] + a_scale_factor * stat_list[1] + \
-            t_switch_factor * stat_list[2] + t_vault_factor * stat_list[3] + t_scale_factor * stat_list[4]
+    score = a_cargo_factor * stat_list[0] + a_hatch_factor * stat_list[1] + \
+            t_cargo_factor * stat_list[2] + t_hatch_factor * stat_list[3] + t_climb_factor * stat_list[4]
 
 
     overall_team_sheet.cell(row, 7).value = score
@@ -194,43 +229,65 @@ def calc_avg(output_sheet, matches_played):
     # AUTO
     # ----------------------------
     # average switch
-    a_switch_sum = 0
-    a_scale_sum = 0
+    a_cargo_sum = 0
+    a_hatch_sum = 0
   # finds sum of all the switch and scale cubes
     for i in range(4, 4 + matches_played, 1):
-        a_switch_sum += output_sheet.cell(i,2).value
-        a_scale_sum += output_sheet.cell(i,3).value
+        a_cargo_sum += output_sheet.cell(i,3).value + output_sheet.cell(i,4).value + output_sheet.cell(i,5).value
+        a_hatch_sum += output_sheet.cell(i,6).value + output_sheet.cell(i,7).value + output_sheet.cell(i,8).value
 # prints out average to the Excel sheet
     if matches_played == 0:
-        output_sheet.cell(4, 7).value = 0
-        output_sheet.cell(4, 8).value = 0
+        output_sheet.cell(4, 10).value = 0
+        output_sheet.cell(4, 11).value = 0
     else:
-        output_sheet.cell(4, 7).value = float(a_switch_sum)/matches_played
-        output_sheet.cell(4, 8).value = float(a_scale_sum)/matches_played
+        output_sheet.cell(4, 10).value = float(a_cargo_sum)/matches_played
+        output_sheet.cell(4, 11).value = float(a_hatch_sum)/matches_played
 
 
     # TELE
     # ------------------------------
-    t_switch_sum = 0
-    t_vault_sum = 0
-    t_scale_sum = 0
+    # OUTPUT SHEET
+    # col 1 - match num
+    # col 2 - Balls low
+    # col 3 - Balls mid
+    # col 4 - balls high
+    # col 5 - hatch low
+    # col 6 - hatch mid
+    # col 7 - hatch high
+    # col 8 - climb
+    # col 9 - notes
+
+    # col 11 - avg switch
+    # col 12 - avg vault
+    # col 13 - avg scale
+    t_cargo_sum = 0
+    t_hatch_sum = 0
+    t_climb_sum = 0
 
     for i in range(21, 21 + matches_played, 1):
-        t_switch_sum += output_sheet.cell(i, 2).value
-        t_vault_sum += output_sheet.cell(i, 3).value
-        t_scale_sum += output_sheet.cell(i, 4).value
+        t_cargo_sum += output_sheet.cell(i, 2).value + output_sheet.cell(i, 3).value + output_sheet.cell(i, 4).value
+        t_hatch_sum += output_sheet.cell(i, 5).value + output_sheet.cell(i, 6).value + output_sheet.cell(i, 7).value
+
+        if output_sheet.cell(i, 8).value == 1:
+            t_climb_sum += 3
+        elif output_sheet.cell(i, 8).value == 2:
+            t_climb_sum += 6
+        elif output_sheet.cell(i, 8).value == 3:
+            t_climb_sum += 12
+        else:
+            t_climb_sum += 0
 
     if matches_played == 0:
-        output_sheet.cell(21, 10).value = 0
-        output_sheet.cell(21, 11).value = 0
-        output_sheet.cell(21, 12).value = 0
+        output_sheet.cell(21, 13).value = 0
+        output_sheet.cell(21, 14).value = 0
+        output_sheet.cell(21, 15).value = 0
     else:
-        output_sheet.cell(21, 10).value = float(t_switch_sum)/matches_played
-        output_sheet.cell(21, 11).value = float(t_vault_sum)/matches_played
-        output_sheet.cell(21, 12).value = float(t_scale_sum)/matches_played
+        output_sheet.cell(21, 13).value = float(t_cargo_sum)/matches_played
+        output_sheet.cell(21, 14).value = float(t_hatch_sum)/matches_played
+        output_sheet.cell(21, 15).value = float(t_climb_sum)/matches_played
 
-    return [output_sheet.cell(4, 7).value, output_sheet.cell(4, 8).value,
-            output_sheet.cell(21, 10).value, output_sheet.cell(21, 11).value , output_sheet.cell(21, 12).value]
+    return [output_sheet.cell(4, 10).value, output_sheet.cell(4,11).value,
+            output_sheet.cell(21, 13).value, output_sheet.cell(21, 14).value , output_sheet.cell(21, 15).value]
 
 
 
@@ -257,7 +314,7 @@ def calc_avg(output_sheet, matches_played):
 
 # -----------------------------------------------------------------------------------
                                  #   INPUT FILE NAME
-data = openpyxl.load_workbook('2018_BB_Data.xlsx')           # <<< Change Name of Input File
+data = openpyxl.load_workbook('2019_LosAngelesRegional_Data.xlsx')           # <<< Change Name of Input File
 
 # -----------------------------------------------------------------------------------
 # auto sheet of the scouting data Excel file
@@ -273,7 +330,7 @@ teamOutput = openpyxl.Workbook()
 
 # -----------------------------------------------------------------------------------
                                 #    OUTPUT FILE NAME
-teamOutput_name = '2018BeachBlitzDataAnalysis1.xlsx'             # <<< Change Name of Output File
+teamOutput_name = '2019LosAngelesDataAnalysis.xlsx'             # <<< Change Name of Output File
 
 # -----------------------------------------------------------------------------------
 
@@ -286,11 +343,11 @@ teamSheet.cell(1,4).value = "Overall Team Data"
     # AUTO
 
 teamSheet.cell(2, 1).value = "Team Num"
-teamSheet.cell(2, 2).value = "Auto Switch AVG"
-teamSheet.cell(2, 3).value = "Auto Scale AVG"
-teamSheet.cell(2, 4).value = "Tele Switch AVG"
-teamSheet.cell(2, 5).value = "Tele Vault AVG"
-teamSheet.cell(2, 6).value = "Tele Scale AVG"
+teamSheet.cell(2, 2).value = "Auto Hatch AVG"
+teamSheet.cell(2, 3).value = "Auto Cargo AVG"
+teamSheet.cell(2, 4).value = "Tele Hatch AVG"
+teamSheet.cell(2, 5).value = "Tele Cargo AVG"
+teamSheet.cell(2, 6).value = "Tele Climb AVG"
 teamSheet.cell(2, 7).value = "Score"
 
 
@@ -300,11 +357,15 @@ teamSheet.cell(2, 7).value = "Score"
 # in data excel sheet
 # col 2 - team num
 # col 3 - match num
-# col 4 - cubes in switch
-# col 5 - cubes in vault
-# col 6 - cubes in scale
-# col 7 - Climb (Yes/No)
-# col 8 - Result (Win/Lost/Tie)
+# col 4 - Balls low
+# col 5 - Balls mid
+# col 6 - balls high
+# col 7 - hatch low
+# col 8 - hatch mid
+# col 9 - hatch high
+# col 10 - climb
+# col 11 - notes
+
 
 # --------------------
 # Set up First team
@@ -324,9 +385,9 @@ played = 0
 
 team_count = 0
 # data to keep overall track of
-teams_switch_avg = []
-teams_scale_avg = []
-teams_vault_avg = []
+teams_cargo_avg = []
+teams_hatch_avg = []
+teams_climb_avg = []
 # format of output sheet
 # col 1 - match num
 # col 2 - switch
